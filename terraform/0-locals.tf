ff61a14059cdfc5d5bc480dfbbcd1e93b79b9af8
iam_mamadou <momodouba44@gmail.com>
2024-10-08 15:37:55 +0000

    fea: Add Terraform file for eks

---
+++ terraform/0-locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env = "dev"
+  region = "eu-west-1"
+  az1= "eu-west-1a"
+  az2 = "eu-west-1b"
+  eks_name = "demo"
+  eks_version = "1.30"
+}
---
+++ terraform/7-eks.tf
@@ -0,0 +1,51 @@
+// Creation du role pour eks
+resource "aws_iam_role" "eks" {
+  name = "${local.env}-${local.eks_name}-eks-cluster"
+
+  assume_role_policy = <<POLICY
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  POLICY
+}
+
+// Attachement de la policy au role
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks.name
+}
+
+// Creation du cluster eks
+
+resource "aws_eks_cluster" "eks" {
+  name = "${local.env}-${local.eks_name}"
+  version = local.eks_version
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access = true
+
+    subnet_ids = [ 
+        aws_subnet.private_zone1.id,
+        aws_subnet.private_zone2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [ aws_iam_role_policy_attachment.eks ]
+
+}
